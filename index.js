653068513cc1679b2ce2a2d976970946ac2566ba
iftekharr001 <iftekharr001@gmail.com>
2023-12-21 11:52:44 -0800

    added some features

--- index.js
+++ index.js
@@ -33,65 +33,85 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
+    const categoryCollection = client
+      .db("libraryManagement")
+      .collection("category");
+    const bookCollection = client.db("libraryManagement").collection("book");
+    const borrowCollection = client
+      .db("libraryManagement")
+      .collection("borrow");
 
-    const categoryCollection = client.db('libraryManagement').collection('category');
-    const bookCollection = client.db('libraryManagement').collection('book');
-    const borrowCollection = client.db('libraryManagement').collection('borrow')
-
-
-    
-
-
-
-    app.get('/borrow', async(req, res) => {
-        let query = {};
-        if(req.query?.email){
-          query = {email: req.query.email}
-        }
+    app.get("/borrow", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
       const result = await borrowCollection.find(query).toArray();
-    
 
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-    app.post('/borrow', async(req, res) => {
+    app.post("/borrow", async (req, res) => {
       const borrows = req.body;
       console.log(borrows);
       const result = await borrowCollection.insertOne(borrows);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-    app.delete('/borrow/:id', async(req, res) => {
+    app.delete("/borrow/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await borrowCollection.deleteOne(query)
-      res.send(result)
-    })
-
+      const query = { _id: new ObjectId(id) };
+      const result = await borrowCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // category collection
-    app.get('/category', async(req, res) => {
-        const cursor = categoryCollection.find();
-        const result = await cursor.toArray();
-        res.send(result)
-    })
+    app.get("/category", async (req, res) => {
+      const cursor = categoryCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     // book collection
 
-    app.post('/books', async(req, res) => {
-        const newBook = req.body;
-        console.log(newBook)
-        const result = await bookCollection.insertOne(newBook)
-        res.send(result)
-    })
+    app.post("/books", async (req, res) => {
+      const newBook = req.body;
+      console.log(newBook);
+      newBook.quantity = parseInt(newBook.quantity, 10);
+      const result = await bookCollection.insertOne(newBook);
+      res.send(result);
+    });
 
-    app.get('/books', async(req, res) => {
-        const cursor = bookCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+    // updated quantity
 
-    
+    // In your server code (e.g., library-management-system-server-steel)
+    app.put("/updateQuantity/:id", async (req, res) => {
+      const bookId = req.params.id;
+      const update = req.body.$inc;
+
+      try {
+        const updatedBook = await bookCollection.updateOne(
+          { _id: new ObjectId(bookId) },
+          { $inc: update }
+        );
+
+        if (updatedBook.matchedCount === 0) {
+          return res.status(404).json({ error: "Book not found" });
+        }
+
+        res.json({ message: "Book quantity updated successfully" });
+      } catch (error) {
+        console.error("Error updating book quantity:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+
+    app.get("/books", async (req, res) => {
+      const cursor = bookCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     app.get("/books/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -99,26 +119,26 @@ async function run() {
       res.send(result);
     });
 
-    app.put('/books/:id', async(req, res) => {
-        const id = req.params.id;
-        const filter = { _id: new ObjectId(id) };
-        const options = { upsert: true };
-        const updatedBooks = req.body;
-        const books = {
-            $set: {
-                name: updatedBooks.name,
-                authorName: updatedBooks.authorName,
-                image: updatedBooks.image,
-                bookCategory: updatedBooks.bookCategory,
-                quantity: updatedBooks.quantity,
-                description: updatedBooks.description,
-                rating: updatedBooks.rating,
-
-            }
-        }
-        const result = await bookCollection.updateOne(filter, books, options);
-        res.send(result)
-    })
+    app.put("/books/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedBooks = req.body;
+       const quantity = parseInt(updatedBooks.quantity, 10);
+      const books = {
+        $set: {
+          name: updatedBooks.name,
+          authorName: updatedBooks.authorName,
+          image: updatedBooks.image,
+          bookCategory: updatedBooks.bookCategory,
+          quantity: quantity,
+          description: updatedBooks.description,
+          rating: updatedBooks.rating,
+        },
+      };
+      const result = await bookCollection.updateOne(filter, books, options);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
